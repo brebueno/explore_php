85f7fcf9014a99faae4ceee3ec0c4fe37e19ee44
Breno Gullo <81334230+brebueno@users.noreply.github.com>
2022-10-21 10:33:09 -0300

    Rev_a

---
+++ processa.php
@@ -0,0 +1,54 @@
+<?php
+
+//recebe os campos do formulário
+$nome = $_POST ['nome'];
+$email = $_POST ['email'];
+$sexo = $_POST ['sexo'];
+$telefone = $_POST ['telefone'];
+$senha = $_POST ['senha'];
+$estado = $_POST ['estado'];
+$cidade = $_POST ['cidade'];
+$destinos = $_POST ['destinos'];
+$idade = $_POST ['idade'];
+$hospedagem = $_POST ['hospedagem'];
+$dt_cadastro = $_POST ['dt-cadastro'];
+$mensagem = $_POST ['mensagem'];
+
+
+//Para investigar variáveis e expressões
+//var_dump($_POST);
+
+//Conecta ao banco e grava os dados (insert com PDO)
+try{
+
+    //Instancia o banco por meio do PDO
+    $pdo = new PDO('mysql:host=localhost;dbname=explore', 'root', '');
+    //insert na tabela users
+    $sql = $pdo->prepare('insert into users_2 
+    (nome, email, sexo, telefone, senha, estado, cidade, destinos, idade, hospedagem, dt_cadastro, mensagem) 
+    values(:nome, :email, :sexo, :telefone, :senha, :estado, :cidade, :destinos, :idade, :hospedagem, :dt_cadastro, :mensagem)');
+    $sql->execute(array(
+
+        ':nome' => $nome,
+        ':email' => $email,
+        ':sexo' => $sexo,
+        ':telefone' => $telefone,
+        ':senha' => md5($senha),
+        ':estado' => $estado,
+        ':cidade' => $cidade,
+        ':destinos' => $destinos,
+        ':idade' => $idade,
+        ':hospedagem' => $hospedagem,
+        ':dt_cadastro' => date('Y-m-d', strtotime($dt_cadastro)),
+        ':mensagem' => $mensagem,
+
+        ));
+
+    echo '<h1> usuario cadastrado </h1>';
+    var_dump($_POST);
+
+} catch (PDOException $erro) {
+    //se der erro, exibe o erro aqui
+    echo $erro;
+
+}
